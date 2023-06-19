b696424647ba717bef4508d279fc1fdb582bd20e
Muhammad Usama <imusamairshad@gmail.com>
2023-06-20 00:59:15 +0500

    category_grid_item.dart is modified.

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,12 +3,17 @@ import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
   final Category category;
+  final void Function() onSelectCategory;
 
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16.0),
       child: Container(
