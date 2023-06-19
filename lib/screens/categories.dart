2daff4d673a300773a6f04e9c9bb7dc9ce2601c8
Muhammad Usama <imusamairshad@gmail.com>
2023-06-19 14:58:16 +0500

    categories.dart is modified.

---
+++ lib/screens/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // Alternative approach: availableCategories.map((category) =>
+          //CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
