1b1ad3b68e2c693486b8df252685d04b19a56be2
Muhammad Usama <imusamairshad@gmail.com>
2023-06-19 14:58:42 +0500

    category_grid_item.dart is added.

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryGridItem({super.key, required this.category});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16.0),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
