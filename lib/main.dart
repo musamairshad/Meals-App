c0668839900b388922cae6c1bcfbb7d55822eda5
Muhammad Usama <imusamairshad@gmail.com>
2023-06-19 14:55:08 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/categories.dart';
+import 'package:meals_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
