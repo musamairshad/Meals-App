2b27ee7cd1fd722abe6ce66bcaa1ba3e4b944e14
Muhammad Usama <imusamairshad@gmail.com>
2023-06-20 23:38:34 +0500

    categories.dart is modified.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          title: 'Some title',
-          meals: [],
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     ); // Navigator.push(context, route);
@@ -38,7 +43,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
