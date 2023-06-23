3a74893d565ec1ada37d5f9a7a21d4c6674ca394
Muhammad Usama <imusamairshad@gmail.com>
2023-06-23 23:10:39 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/tabs.dart';
+// import 'package:meals_app/screens/categories.dart';
 // import 'package:meals_app/screens/meals.dart';
 // import 'package:meals_app/data/dummy_data.dart';
 
@@ -25,7 +26,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
+      // home: const CategoriesScreen(),
       // home: const MealsScreen(
       //   title: 'Some category...',
       //   meals: dummyMeals,
