1373592d2ee70bf3930b9f3ceda6fa71a23cfd46
Muhammad Usama <imusamairshad@gmail.com>
2023-06-24 15:19:28 +0500

    meal_details.dart is modified.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -3,14 +3,24 @@ import 'package:meals_app/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
-  const MealDetailsScreen({super.key, required this.meal});
+  const MealDetailsScreen(
+      {super.key, required this.meal, required this.onToggleFavorite});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.star),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
