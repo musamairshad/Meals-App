9022e50de9ab081f6cba0891b192aa4ce891e6c9
Muhammad Usama <imusamairshad@gmail.com>
2023-06-28 17:42:50 +0500

    filters.dart is modified.

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -10,7 +10,8 @@ enum Filter {
 }
 
 class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key});
+  final Map<Filter, bool> currentFilters;
+  const FiltersScreen({super.key, required this.currentFilters});
   @override
   State<FiltersScreen> createState() {
     return _FiltersScreenState();
@@ -22,6 +23,15 @@ class _FiltersScreenState extends State<FiltersScreen> {
   var _lactoseFreeFilterSet = false;
   var _vegetarianFilterSet = false;
   var _veganFilterSet = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currentFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = widget.currentFilters[Filter.vegetarian]!;
+    _veganFilterSet = widget.currentFilters[Filter.vegan]!;
+  }
 
   @override
   Widget build(BuildContext context) {
