5349ffaa057fb061ba3c4568652597aa9dac14cf
Muhammad Usama <imusamairshad@gmail.com>
2023-07-01 00:49:59 +0500

    favorites_provider.dart is modified.

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -4,13 +4,16 @@ import 'package:meals_app/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  // Before, this method was returning void.
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
