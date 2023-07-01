acfe11d643c44bfbe2d7386cecb33752524df9e0
Muhammad Usama <imusamairshad@gmail.com>
2023-07-01 16:22:11 +0500

    meal_details.dart is modified.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -15,6 +15,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -34,7 +38,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
